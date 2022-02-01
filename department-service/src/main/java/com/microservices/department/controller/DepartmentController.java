4ef6d691d85ce1edd278138267e8ad4abcd52b40
Alan Santos <alanjosedossantos011@gmail.com>
2022-02-01 15:30:39 -0300

    department findById controller

--- department-service/src/main/java/com/microservices/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/microservices/department/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import com.microservices.department.entity.Department;
 import com.microservices.department.service.DepartmentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,12 @@ public class DepartmentController {
     public Department save(@RequestBody Department department) {
         log.info("Inside saveDepartment method of DepartmentController.");
         return service.save(department);
+    }
+
+    @GetMapping("{id}")
+    public Department findById(@PathVariable Long id) {
+        log.info("inside findById method of Department");
+        return service.findById(id);
+
     }
 }
